9f0e27a50e87c09e226944b807967a0c16d6891d
gurvinder <47gurvinder@gmail.com>
2015-06-24 23:59:01 +0530

    Database

--- app/src/main/java/com/rajpal/books/Database/Database.java
+++ app/src/main/java/com/rajpal/books/Database/Database.java
@@ -13,8 +13,21 @@ public class Database extends SQLiteOpenHelper {
     public static final int VERSION = 1;
     public static final String DB_NAME = "m.db";
     public static final String TABLE_Downloads = "downloads";
+    public static final String Download_COLUMN_id = "id";
+    public static final String Download_COLUMN_path = "path";
+    public static final String Download_COLUMN_name = "name";
+    public static final String Download_COLUMN_time = "time";
+    public static final String Download_COLUMN_content_length = "c_lenght";
+    public static final String Download_COLUMN_download_size = "size";
+    public static final String Download_COLUMN_last_modified = "last_modified";
+    public static final String Download_COLUMN_downloading_status = "status";
 
-
+    String CreateTable = "create table " + TABLE_Downloads + " (" + Download_COLUMN_id
+            + " integer primary key, " + Download_COLUMN_name + " text, "
+            + Download_COLUMN_path + " text, " + Download_COLUMN_time
+            + " text, " + Download_COLUMN_content_length + " text, " + Download_COLUMN_download_size
+            + " text, " + Download_COLUMN_last_modified + " text, "
+            + Download_COLUMN_downloading_status + " text)";
 
     public Database(Context context) {
         super(context, DB_NAME, null, VERSION);
@@ -22,7 +35,7 @@ public class Database extends SQLiteOpenHelper {
 
     @Override
     public void onCreate(SQLiteDatabase db) {
-db.execSQL("");
+        db.execSQL(CreateTable);
     }
 
     @Override
